20c6ffd8d088ae898ee50f99ba3c4447c907cb77
Thesonggodoftoliet <535644752@qq.com>
2019-04-29 11:21:16 +0800

    更新learn服务类，完成错题获取
    1.learn类新增函数
    2.完成单道错题获取
    3.完成所有错题获取

--- src/com/sheepclass/servlet/homework/getAllMistakes.java
+++ src/com/sheepclass/servlet/homework/getAllMistakes.java
@@ -1,7 +1,19 @@
 package com.sheepclass.servlet.homework;
 
+import com.sheepclass.entity.Course;
+import com.sheepclass.entity.Homework;
+import com.sheepclass.entity.Mistakes;
+import com.sheepclass.service.Learn;
+import com.sheepclass.utils.JwtUtils;
+import com.sheepclass.utils.ReciveUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -15,6 +27,57 @@ public class getAllMistakes extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("getAllMistakes");
+        JSONObject jsonObject = ReciveUtils.getObject(request);
+        JSONObject msg = new JSONObject();
+        String token = null;
+        int userid = 0;
+        try{
+            token = jsonObject.getString("token");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        if (JwtUtils.verifyToken(token) == 0){
+            try{
+                msg.put("tag",0);
+                msg.put("token"," ");
+                msg.put("mistakes"," ");
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }else {
+            userid = JwtUtils.decodeToken(token);
+            token = JwtUtils.createToken(userid);
+            Learn learn = new Learn();
+            List<Homework> mistakes = learn.getMistakesByuserid(userid);
+            List<String> coursename = learn.getNameofMistakesByuserid(userid);
+            List<Mistakes> mistakesList = learn.getMistakes(userid);
+            JSONArray jsonArray = new JSONArray();
+            try {
+                for (int i = 0; i < mistakes.size(); i++) {
+                    JSONObject mistake = new JSONObject();
+                    mistake.put("homeworkid",mistakes.get(i).getHomeworkid());
+                    mistake.put("content",mistakes.get(i).getContent());
+                    mistake.put("correct",mistakes.get(i).getCorrect());
+                    mistake.put("A",mistakes.get(i).getA());
+                    mistake.put("B",mistakes.get(i).getB());
+                    mistake.put("C",mistakes.get(i).getC());
+                    mistake.put("D",mistakes.get(i).getD());
+                    mistake.put("coursename",coursename.get(i));
+                    mistake.put("times",mistakesList.get(i).getWrongtimes());
+                    jsonArray.put(mistake);
+                }
+                msg.put("tag",1);
+                msg.put("token",token);
+                msg.put("mistakes",jsonArray);
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        PrintWriter out = response.getWriter();
+        out.print(msg);
+        out.flush();
+        out.close();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
