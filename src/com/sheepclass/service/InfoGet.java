081e73dfb13f8903bdbd73b4f734c537ec2640c9
Thesonggodoftoliet <535644752@qq.com>
2019-05-17 11:32:41 +0800

    增加获取错误知识点service
    1.更正了获取错误知识点和错误课程可能出现越界的情况

--- src/com/sheepclass/service/InfoGet.java
+++ src/com/sheepclass/service/InfoGet.java
@@ -1,16 +1,9 @@
 package com.sheepclass.service;
 
-import com.sheepclass.dao.CourseDao;
-import com.sheepclass.dao.LearninginfoDao;
-import com.sheepclass.dao.MistakesDao;
-import com.sheepclass.dao.UserDao;
-import com.sheepclass.dao.implement.CourseDaoImle;
-import com.sheepclass.dao.implement.LearninginfoDaoimple;
-import com.sheepclass.dao.implement.MistakesDaoImpl;
-import com.sheepclass.dao.implement.UserDaoIm;
-import com.sheepclass.entity.Course;
-import com.sheepclass.entity.Mistakes;
-import com.sheepclass.entity.Users;
+import com.sheepclass.dao.*;
+import com.sheepclass.dao.implement.*;
+import com.sheepclass.entity.*;
+import com.sheepclass.utils.PraseUtils;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -20,7 +13,7 @@ import java.util.List;
 public class InfoGet {
     //学情管理，包含预警
     //七天一个周期
-    public int getWeeklogintimes(int userid){
+    public int getWeeklogintimes(int userid){//一周打卡的次数
         Calendar calendar = Calendar.getInstance();
         calendar.getFirstDayOfWeek();
         LearninginfoDao learninginfoDao = new LearninginfoDaoimple();
@@ -76,9 +69,71 @@ public class InfoGet {
         List<Course> courses = new ArrayList<>();
         CourseDao courseDao = new CourseDaoImle();
         for (int i = 0;i<3;i++){
+            if (temps.size()==i)
+                break;
             courses.add(courseDao.getcourseByid(temps.get(i).getCourseid()));
         }
         return courses;
+    }
+
+    public List<Knowledge> getMostwrongKnowledges(int userid) {
+        class Temp implements Comparable{
+            int id;
+            int count;
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public int getCount() {
+                return count;
+            }
+
+            public void setCount(int count) {
+                this.count = count;
+            }
+
+            @Override
+            public int compareTo(Object o) {
+                Temp p = (Temp)o;
+                return p.count-this.count;
+            }
+        }
+        MistakesDao mistakesDao = new MistakesDaoImpl();
+        HomeworkDao homeworkDao = new HomeworkDaoImpl();
+        KnowledgeDao knowledgeDao = new KnowledgeDaoImpl();
+        List<Mistakes> mistakes = mistakesDao.getMistakesByuserid(userid);
+        List<Homework> homeworks = new ArrayList<>();
+        List<Integer> point = new ArrayList<>();
+        for (int i = 0; i < mistakes.size(); i++) {
+            homeworks.add(homeworkDao.getHomeworkById(mistakes.get(i).getHomeworkid()));
+        }
+
+        for (int j = 0; j < homeworks.size(); j++) {
+            point.addAll(PraseUtils.sToi(homeworks.get(j).getSets()));//知识点编号集
+        }
+
+        Collections.sort(point);
+        List<Temp>temps = new ArrayList<>();
+        for (int m =0;m<point.size();m++){
+            Temp temp = new Temp();
+            temp.setId(point.get(m));
+            int last = point.lastIndexOf(temp.getId());
+            temp.setCount(last-m+1);
+            m=last+1;
+            temps.add(temp);
+        }
+        List<Knowledge>knowledges = new ArrayList<>();
+        for (int i=0;i<5;i++){
+            if (temps.size()==i)
+                break;
+            knowledges.add(knowledgeDao.getKnowledgeByid(temps.get(i).getId()));
+        }
+        return knowledges;
     }
 
 }
