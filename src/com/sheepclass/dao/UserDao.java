c3a27adce50b8b9f9623e4317e2aed59c6b699a7
Thesonggodoftoliet <535644752@qq.com>
2019-05-21 09:42:37 +0800

    开始完善学习提醒
    1.完成邮件发送模块
    2.为UserDao添加身份查询

--- src/com/sheepclass/dao/UserDao.java
+++ src/com/sheepclass/dao/UserDao.java
@@ -2,11 +2,14 @@ package com.sheepclass.dao;
 
 import com.sheepclass.entity.Users;
 
+import java.util.List;
+
 
 public interface UserDao {
     Users getUserById(int userid);
     Users getUserByphone(String phone);
     Users getUserByEmail(String email);
+    List<Users> getUsersByIdentity(int identity);
     int addUser(Users user);
     int setUser(Users user);
     int updateTotTime(Users user);
